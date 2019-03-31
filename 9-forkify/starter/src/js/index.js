37c7915515993076c3b5081df72ec2982460bd17
EliTu <eliad91@gmail.com>
2019-03-31 16:09:50 +0800

    Added lecture 135, configured Babel

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,5 @@
 // Global app controller
 import x from './test';
+const y = 23;
 console.log(`I imported ${x} from another module!`);
-console.log(`This is another test ${x}`);
+console.log(`This is another test to check babel with the variable ${y}`);
--- 9-forkify/starter/webpack.config.js
+++ 9-forkify/starter/webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['@babel/polyfill', './src/js/index.js'],
+
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
@@ -17,5 +18,15 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    }
 };
