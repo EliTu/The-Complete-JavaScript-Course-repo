4afaf578925984ef60964cc77915d320883f1329
EliTu <eliad91@gmail.com>
2019-04-10 10:24:00 +0800

    Finished lecture 141, updated the search function in the Controller model

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,42 @@
+// Imports:
 import Search from './models/Search';
 
-const search = new Search('hamburger');
+// The global state variable:
+const state = {
+    /* Global state of the app:
+    - Search object
+    - Current recpie object
+    - Shopping list object
+    - Liked recipe
+    */
+};
 
-console.log(search);
-search.getResults();
+// Search input event listeners:
+
+const controlSearch = async (e) => {
+
+    // Prevent the page from reloading upon pressing the search button:
+    e.preventDefault();
+
+    // Get the query from the view:
+    const searchFieldValue = document.querySelector('.search__field').value;
+
+    const query = searchFieldValue; // To be moved to the view model eventually 
+
+    if (query) {
+        // New search object, and add it to state:
+        state.search = new Search(query);
+
+        // Prepare the UI for the results:
+
+        // Search for recipes by making an API call:
+        await state.search.getResults();
+
+        // Render results on UI:
+        console.log(state.search.result);
+    }
+}
+
+const searchInput = document.querySelector('.search');
+
+searchInput.addEventListener('submit', controlSearch);
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -1,17 +1,25 @@
+// Imports:
 import axios from 'axios';
+
+// Exporting a search class:
 export default class Search {
     constructor(query) {
         this.query = query;
     }
 
+    // Async search function:
     async getResults() {
+        // Personal API key value:
         const key = `96211b9a697ef36fbdc9702b9112102e`;
+        // CORS proxy address:
         const corsProxy = 'https://cors-anywhere.herokuapp.com/';
         try {
+            // The API request function with axios:
             const result = await axios(`${corsProxy}www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            // Pass the result as a property of the class:
             this.result = result.data.recipes;
-            console.log(this.result);
         } catch (error) {
+            // Error handling:
             console.log('Error!');
         }
     }
